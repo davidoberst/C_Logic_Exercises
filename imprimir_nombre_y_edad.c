f9c532987f4f5aab6d7408fa8f44893290f752aa
davidoberst <juandavidbustosperez82@gmail.com>
2025-10-04 03:04:08 -0500

    Imprimir nombre y edad

---
+++ imprimir_nombre_y_edad.c
@@ -0,0 +1,8 @@
+//imprimir nombre y edad
+#include<stdio.h>
+int main(){
+    char nombre[] = ("davidoberst");
+    int age = 999;
+    printf("mi nombre es %s y mi edad es %d\n",nombre,age);
+    return 0;
+}
