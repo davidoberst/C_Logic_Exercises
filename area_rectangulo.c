2b910450bb8e53adb0afca0a99cf3dd0181f7ccd
davidoberst <juandavidbustosperez82@gmail.com>
2025-10-04 23:15:26 -0500

    Calcular area de un rectangulo

---
+++ area_rectangulo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int base = 80;
+    int altura = 70;
+    printf("El area del rectangulo es %d\n ", base * altura);
+    system("pause");
+    return 0;
+    
+}
