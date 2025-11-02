879dfe9e87b9793a871a496227d6faa384d5288d
davidoberst <juandavidbustosperez82@gmail.com>
2025-11-02 12:04:59 -0500

    contador de vocales

--- contarVocales.c
+++ contarVocales.c
@@ -3,17 +3,19 @@ int VocalsCounter(char word[]){
  char vocales[] = {'a', 'e', 'i', 'o', 'u'};
  printf("Palabra : ");
  scanf("s%",word);    
- for(int i=0;word[i];i++){
-  if(){
-       
-  }//end if
-           
- } //end for
+ int arrSize = sizeof(vocales) / sizeof(vocales[0]);
+ for(int x=0;word[x] !='\0'; x++){
+  for(int y=0; y<arrSize; y++){
+    if(word[x]== y){
+     y++;
+     printf("El total de vocales en la palabra %s es %d",word,y);                     
+    }//end if              
+  }//end for2        
+ } //end for1
 }//end function
 
-
-
 int main(){   
- VocalsCounter("hola");
+ VocalsCounter("hola"); 
  return 0;
 }//end main
+
