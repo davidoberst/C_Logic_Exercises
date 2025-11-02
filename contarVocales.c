5458ac486e26d6c0b71ea1759adf93b28ab69403
davidoberst <juandavidbustosperez82@gmail.com>
2025-11-02 11:43:44 -0500

    Contar Vocales en una palabra

---
+++ contarVocales.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int VocalsCounter(char word[]){
+ char vocales[] = {'a', 'e', 'i', 'o', 'u'};
+ printf("Palabra : ");
+ scanf("s%",word);    
+ for(int i=0;word[i];i++){
+  if(){
+       
+  }//end if
+           
+ } //end for
+}//end function
+
+
+
+int main(){   
+ VocalsCounter("hola");
+ return 0;
+}//end main
