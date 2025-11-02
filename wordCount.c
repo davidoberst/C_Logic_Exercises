c20741b388508185ba3c51176eca9dfbcb409f71
davidoberst <juandavidbustosperez82@gmail.com>
2025-11-02 01:44:36 -0500

    Contar cuantas letras tiene una palabra

---
+++ wordCount.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//Contar cuantas letras tiene una palabra
+int count(char word[]){ 
+ int c = 0;
+ for(int i=0; word[i] != '\0'; i++){
+  c++;
+ }    
+ return c;
+}    
+
+int main(){    
+ char w[100];
+ printf("Write your word : ");
+ scanf("%s",w);
+ int functionCall = count(w);
+ printf("El total de letras en %s es : %d \n",w,functionCall);
+ system("pause");
+    return 0;
+}
