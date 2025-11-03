7df47c4fb5edf2e99ed61a2eec2df083a7dd6a07
davidoberst <juandavidbustosperez82@gmail.com>
2025-11-03 13:08:58 -0500

    Sumar los numeros del 1 al 100

---
+++ sumNums.c
@@ -0,0 +1,9 @@
+//sumar todos los numeros del 1 al 100
+int main(){
+ int total = 0;
+ for(int x = 0; x<101; x++){
+  total = total + x;
+  printf("%d \n",total + x);       
+   }  
+ system("pause");    
+}
