6ed8f7281109986e1a0667e66c81ad04f807ca1e
davidoberst <juandavidbustosperez82@gmail.com>
2025-11-05 16:19:57 -0500

    invertir un String

--- invertirString.c
+++ invertirString.c
@@ -1,8 +1,11 @@
 //invertir una cadena
 #include<stdio.h>
 int main(){
- char string = "holamundo";
- printf("s \n",string);
- system("pause");
-    
+ char word[] = "holamundo";
+ int size = sizeof(word) /sizeof(word[0]);
+ printf("%d\n",size);
+ for (int i = size - 2; i >= 0; i--) {
+  printf("%c \n",word[i]);
+ }
+ system("pause"); 
 }
