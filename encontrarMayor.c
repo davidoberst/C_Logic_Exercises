8c5ce350ed8db6193c38ced39499593ae481ade2
davidoberst <juandavidbustosperez82@gmail.com>
2025-11-06 20:30:48 -0500

    encontrar el numero mayor de un array

--- encontrarMayor.c
+++ encontrarMayor.c
@@ -2,9 +2,13 @@
 #include <stdio.h>
 int main(){
  int list[]={1,2,3,4,20,5,9,13};    
+ int mayor = list[0];
  int size = sizeof(list) / sizeof(list[0]);
  for(int x=0; x<size ;x++){
-  printf("%d\n",list[x]);
+  if(mayor < list[x]){
+   mayor = list[x];
+   }
  }
+ printf("%d",mayor);  
  system("pause");
 }
