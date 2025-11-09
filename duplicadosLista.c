e2c096ef1030e93ba5a790e169aac55ced14d308
davidoberst <juandavidbustosperez82@gmail.com>
2025-11-09 12:25:58 -0500

    Encontrar duplicados de una lista

--- duplicadosLista.c
+++ duplicadosLista.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 int main(){
- int list[] = {1,2,3,4,5,6,7,8,9,10,11,12,10,2};
+ int list[] = {1,2,3,4,5,6,7,8,9,10,11,12,10,2,11};
  int listSize = sizeof(list)/sizeof(list[0]);
  int counterList[listSize];
- int count;
+ int count = 0;
  for(int x=0; x<listSize; x++){
-  counterList[x] = list[x];
-  if(counterList[x] == list[x]){
-   count+1;                 
-  }        
- }
- printf("%d",count);
+  for(int y=x+1; y<listSize; y++){
+    if(list[x]==list[y]){
+     count++;  
+     printf("numero repetido encontrado : %d\n",list[x]);                  
+   }
+  }
+ } 
  system("pause");
 }
