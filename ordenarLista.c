154ed278443cba21b899636c99b09c4b9b22fb58
davidoberst <juandavidbustosperez82@gmail.com>
2025-11-12 19:30:47 -0500

    Ordenar Lista

--- ordenarLista.c
+++ ordenarLista.c
@@ -6,7 +6,7 @@ int main(){
  int size = sizeof(list) / sizeof(list[0]);
  for(int x=0;x<size;x++){
   for(int y=x+1;y<size-1;y++){
-   if(list[x]<list[y]){
+   if(list[y]<list[x]){
     temp = list[x];    
     list[x] = list[y];   
     list[y]= temp;
@@ -14,7 +14,7 @@ int main(){
   }    
  }
  for(int z=0;z<size;z++){
-  printf("%d",list[z]);   
+  printf("%d\n",list[z]);   
  }
  system("pause");
  return 0;
