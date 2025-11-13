7db86552fe573936acfa6b070584099a9c735fc3
davidoberst <juandavidbustosperez82@gmail.com>
2025-11-12 19:19:27 -0500

    Ordenar Lista

--- ordenarLista.c
+++ ordenarLista.c
@@ -1,14 +1,20 @@
 //ordenar una lista
 #include <stdio.h>
 int main(){
+ int temp;
  int list[] ={2,10,4,1,3,80};   
  int size = sizeof(list) / sizeof(list[0]);
  for(int x=0;x<size;x++){
-  for(int y=1;x<size;y++){
-   if(list[x]>list[y]){
-    printf("no");        
+  for(int y=x+1;y<size-1;y++){
+   if(list[x]<list[y]){
+    temp = list[x];    
+    list[x] = list[y];   
+    list[y]= temp;
    }       
   }    
+ }
+ for(int z=0;z<size;z++){
+  printf("%d",list[z]);   
  }
  system("pause");
  return 0;
